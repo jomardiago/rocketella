d1b097783fff427b6f1cf725d7f4310a35831bf3
Jose <Jose.Diago@kyndryl.com>
2023-08-30 12:05:09 +0800

    initial commit

---
+++ app/components/HeaderAction.tsx
@@ -0,0 +1,8 @@
+export default function HeaderAction() {
+  return (
+    <div className="pt-24 flex justify-center gap-28">
+      <button className="bg-neutral-500 text-white py-3 px-6 rounded-md w-[200px]">Talk to an Expert</button>
+      <button className="bg-neutral-500 text-white py-3 px-6 rounded-md w-[200px]">See our Work</button>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        smoke: '#B5ACA3',
+        black: '#1A1713'
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
