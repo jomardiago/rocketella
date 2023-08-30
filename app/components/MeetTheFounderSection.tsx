1a4276b10e08d72dd2d1b8479fd64254a79c1717
Jose <jomardiago@gmail.com>
2023-08-30 18:39:37 +0800

    Changing ' to &apos;

--- app/components/MeetTheFounderSection.tsx
+++ app/components/MeetTheFounderSection.tsx
@@ -29,7 +29,7 @@ export default function MeetTheFounder() {
         <div className={`${josefinsansLight.className} flex-1 text-xl flex gap-6 flex-col`}>
           <h2 className={`${josefinsansBold.className} text-2xl`}>MEET THE FOUNDER</h2>
           <p>
-            Hello, Rocketeers! I'm Yvette Ramos, the founder of this extraordinary marketing and creatives agency. With
+            Hello, Rocketeers! I&apos;m Yvette Ramos, the founder of this extraordinary marketing and creatives agency. With
             over 12 years of experience in the marketing industry, I have witnessed the power of effective marketing
             strategies and their ability to transform businesses. My journey has been shaped by a solid marketing and
             communications background, allowing me to navigate various industries, catering to both B2B and B2C markets.
