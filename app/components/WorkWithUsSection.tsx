5955e4161b89b34ddc52a7fb56d5c3798e062fe3
Jose <jomardiago@gmail.com>
2023-08-31 08:14:32 +0800

    Adding why work with us section

---
+++ app/components/WorkWithUsSection.tsx
@@ -0,0 +1,15 @@
+export default function WhyWorkWithUs() {
+  return (
+    <section className="text-center bg-neutral-500">
+      <div className="contained py-16 px-36 text-xl space-y-4 text-white">
+        <h2 className="text-2xl font-semibold">Why work with us?</h2>
+
+        <p className="font-light">
+          There are many marketing and creative agencies that you can work with.
+          <span className="block">But, there&apos;s only one like us.</span>
+          <span>When you allow Rocketella Studios as your creative and branding companion, you&apos;ll get:</span>
+        </p>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from './components/HeroSection';
 import BrandingSection from './components/BrandingSection';
 import WeTransformSection from './components/WeTransformSection';
 import MeetTheFounder from './components/MeetTheFounderSection';
+import WhyWorkWithUs from './components/WorkWithUsSection';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <BrandingSection />
       <WeTransformSection />
       <MeetTheFounder />
+      <WhyWorkWithUs />
     </main>
   );
 }
