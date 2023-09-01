e87e7bc68f6f4d309c26715fd670ef5470f446da
Jose <Jose.Diago@kyndryl.com>
2023-09-01 10:22:54 +0800

    making branding section responsive

--- app/components/BrandingSection.tsx
+++ app/components/BrandingSection.tsx
@@ -8,14 +8,14 @@ export default function BrandingSection() {
         alt="Creative people working and laughing"
         width={1000}
         height={1000}
-        className="w-full h-[700px] object-cover opacity-40"
+        className="w-full object-cover opacity-30 h-[500px] lg:h-[700px]"
         quality={100}
       ></Image>
-      <div className="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]">
-        <p className="w-[650px] text-center text-2xl">
+      <div className="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] w-full lg:w-[50%]">
+        <p className="w-full text-center text-xl lg:text-2xl lg:w-[650px]">
           A ONE-STOP MARKETING AND CREATIVE AGENCY <span className="block">THAT PRODUCES TOP NOTCH</span>{' '}
           <span className="block">BRANDING SOLUTIONS FOR BUSINESSES,</span>
-          <span className="text-3xl font-semibold">BIG AND SMALL</span>
+          <span className="text-2xl font-semibold lg:text-3xl">BIG AND SMALL</span>
         </p>
       </div>
     </section>
