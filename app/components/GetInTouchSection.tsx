66f3f0c6448913f179643587cd3d64041ee63db5
Jose <Jose.Diago@kyndryl.com>
2023-09-02 12:15:43 +0800

    Making GetInTouchSection responsive

--- app/components/GetInTouchSection.tsx
+++ app/components/GetInTouchSection.tsx
@@ -1,13 +1,13 @@
 export default function GetInTouchSection() {
   return (
-    <section className="flex justify-center py-20 bg-neutral-500 text-cotton">
+    <section className="flex justify-center p-6 bg-neutral-500 text-cotton">
       <div className="flex flex-col justify-center items-center space-y-4">
-        <h2 className="text-3xl text-white">Let&apos;s skyrocket your business today!</h2>
-        <span className="text-center text-xl">
+        <h2 className="text-2xl text-white text-center">Let&apos;s skyrocket your business today!</h2>
+        <span className="text-center text-lg">
           <p>Got a project? Let&apos;s make it happen!</p>
           <p>Give us the details and we&apos; send you a free quote!</p>
         </span>
-        <button className="bg-suede text-white py-3 px-6 rounded-md w-[200px] text-lg">Get in Touch</button>
+        <button className="bg-suede text-white rounded-md w-[200px] text-lg font-normal py-2 px-4 lg:py-3 lg:px-6">Get in Touch</button>
       </div>
     </section>
   );
