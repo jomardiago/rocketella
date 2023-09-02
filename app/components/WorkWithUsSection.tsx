813672882b42d9598eeaccbf3e80d733460e0d7c
Jose <Jose.Diago@kyndryl.com>
2023-09-02 12:03:58 +0800

    Making WorkWithUsSection responsive

--- app/components/WorkWithUsSection.tsx
+++ app/components/WorkWithUsSection.tsx
@@ -1,10 +1,10 @@
 export default function WhyWorkWithUsSection() {
   return (
     <section className="text-center bg-neutral-500">
-      <div className="contained py-16 px-36 text-xl space-y-4 text-white">
-        <h2 className="text-2xl font-semibold">Why work with us?</h2>
+      <div className="contained py-6 px-4 text-xl space-y-4 text-white">
+        <h2 className="text-xl font-semibold">Why work with us?</h2>
 
-        <p className="font-light">
+        <p className="font-light text-lg">
           There are many marketing and creative agencies that you can work with.
           <span className="block">But, there&apos;s only one like us.</span>
           <span>When you allow Rocketella Studios as your creative and branding companion, you&apos;ll get:</span>
