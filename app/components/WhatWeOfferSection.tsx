0a2b697b152ceadc888ea1da59a0c11b57ea26ef
Jose <Jose.Diago@kyndryl.com>
2023-09-02 12:13:21 +0800

    Making WhatWeOfferSection resposive

--- app/components/WhatWeOfferSection.tsx
+++ app/components/WhatWeOfferSection.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image';
 
 export default function WhatWeOfferSection() {
   return (
-    <section className="relative">
+    <section className="">
       <Image
         src="/digital-nomad.jpg"
         alt="A girl smiling and holding a tablet"
         width={1000}
         height={1000}
         quality={100}
-        className="w-full object-cover opacity-70 object-top"
+        className="hidden w-full object-cover opacity-70 object-top"
       />
-      <div className="absolute top-0 p-36 h-full text-2xl">
-        <div className="flex justify-between items-center h-full">
-          <div className="flex-1 flex flex-col gap-32">
+      <div className="p-6 h-full text-lg">
+        <div className="flex flex-col justify-between items-center h-full">
+          <div className="flex-1 flex flex-col gap-4 text-center">
             <div>
               <h2 className="text-3xl font-bold">What we offer</h2>
               <p className="font-light mt-2">
@@ -24,24 +24,24 @@ export default function WhatWeOfferSection() {
 
             <div>
               <h2 className="font-bold">Branding</h2>
-              <button className="bg-neutral-500 text-white py-3 px-6 rounded-md w-[200px] mt-2">Learn More</button>
+              <button className="bg-neutral-500 text-white rounded-md w-[200px] font-normal text-base py-2 px-4 lg:py-3 lg:px-6">Learn More</button>
             </div>
 
             <div>
               <h2 className="font-bold">Digital Marketing Management</h2>
-              <button className="bg-neutral-500 text-white py-3 px-6 rounded-md w-[200px] mt-2">Learn More</button>
+              <button className="bg-neutral-500 text-white rounded-md w-[200px] font-normal text-base py-2 px-4 lg:py-3 lg:px-6">Learn More</button>
             </div>
           </div>
 
-          <div className="flex-1 flex justify-end">
-            <div className="flex flex-col gap-40">
+          <div className="flex-1 flex justify-end mt-4">
+            <div className="flex flex-col gap-4 text-center">
               <div>
                 <h2 className="font-bold">Strategy Consultation</h2>
-                <button className="bg-neutral-500 text-white py-3 px-6 rounded-md w-[200px] mt-2">Learn More</button>
+                <button className="bg-neutral-500 text-white rounded-md w-[200px] py-2 px-4 lg:py-3 lg:px-6">Learn More</button>
               </div>
               <div>
                 <h2 className="font-bold">Content Creation</h2>
-                <button className="bg-neutral-500 text-white py-3 px-6 rounded-md w-[200px] mt-2">Learn More</button>
+                <button className="bg-neutral-500 text-white rounded-md w-[200px] py-2 px-4 lg:py-3 lg:px-6">Learn More</button>
               </div>
             </div>
           </div>
